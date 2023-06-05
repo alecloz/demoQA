6e2c2a1fef3a59c61ceffe62058d33c51acd22fd
Alexey <alec.loz@yandex.ru>
2023-06-06 00:15:20 +0300

    lesson 5 homework: GitHub and dragAndDrop

---
+++ src/test/java/qa/HerokuAppTest.java
@@ -0,0 +1,31 @@
+package qa;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class HerokuAppTest {
+
+    //работает исправно
+    @Test
+    void dragAndDropBySelenideElement() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        WebDriverRunner.getWebDriver().manage().window().maximize();
+        $x("//*[@id='column-a']").shouldHave(text("A"));
+        $("#column-a").hover().dragAndDropTo( $("#column-b"));
+        $x("//*[@id='column-a']").shouldHave(text("B"));
+    }
+
+    //отработал корректно один раз, после чего постоянно падает
+    @Test
+    void dragAndDropByAction() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        WebDriverRunner.getWebDriver().manage().window().maximize();
+        $x("//*[@id='column-a']").shouldHave(text("B"));
+        Selenide.actions().dragAndDrop($("#column-a"),$("#column-b"));
+        $x("//*[@id='column-a']").shouldHave(text("A"));
+    }
+}
