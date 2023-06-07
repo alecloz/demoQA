591c7b35a092014e44d8e5dc77d6d5434bd228d0
Alexey <alec.loz@yandex.ru>
2023-06-07 23:42:09 +0300

    lesson 5 homework: GitHub and dragAndDrop little fix

--- src/test/java/qa/HerokuAppTest.java
+++ src/test/java/qa/HerokuAppTest.java
@@ -6,9 +6,12 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HerokuAppTest {
-
+    Logger logger = LoggerFactory.getLogger(HerokuAppTest.class);
+    static final Logger LOGGER = LoggerFactory.getLogger(HerokuAppTest.class);
     //работает исправно
     @Test
     void dragAndDropBySelenideElement() {
@@ -20,12 +23,14 @@ public class HerokuAppTest {
     }
 
     //отработал корректно один раз, после чего постоянно падает
-    /*@Test
+    @Test
     void dragAndDropByAction() {
+        logger.info("Start Test");
         open("https://the-internet.herokuapp.com/drag_and_drop");
         WebDriverRunner.getWebDriver().manage().window().maximize();
         $x("//*[@id='column-a']").shouldHave(text("B"));
         Selenide.actions().dragAndDrop($("#column-a"),$("#column-b"));
         $x("//*[@id='column-a']").shouldHave(text("A"));
-    }*/
+        logger.info("Finish Test");
+    }
 }
