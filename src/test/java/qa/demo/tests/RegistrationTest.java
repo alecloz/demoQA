9c03c82e8fc09b1a1f358c8ec5dcdb83b880678f
Alexey <alec.loz@yandex.ru>
2023-07-01 21:15:52 +0300

    little fix

--- src/test/java/qa/demo/tests/RegistrationTest.java
+++ src/test/java/qa/demo/tests/RegistrationTest.java
@@ -1,41 +1,67 @@
 package qa.demo.tests;
 
-
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import qa.demo.pages.RegistrationPage;
+import org.openqa.selenium.Keys;
 
-public class RegistrationTest extends BaseTest {
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    RegistrationPage registrationPage = new RegistrationPage();
+public class RegistrationTest {
+
+    @BeforeAll
+    static void preconditionConfiguration() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
 
     @Test
     void testSuccessfulStudentRegistration() {
-        openPage("/automation-practice-form");
-        registrationPage
-                .setFirstName("Ivan")
-                .setLastName("Ivanov")
-                .setUserEmail("iivanov@mail.ru")
-                .setGender("Male")
-                .setUserNumber("9998887766")
-                .setBirthday("15", "December", "1990")
-                .setSubject("English")
-                .setHobby("Reading")
-                .uploadFile("cat.PNG")
-                .setCurrentAddress("www.LeningradSPB.ru")
-                .setState("Uttar Pradesh")
-                .setCity("Agra")
-                .submitClick();
+        open("/automation-practice-form");
+        getWebDriver().manage().window().maximize();
+        adBlockAndFooterRemove();
+        $("#firstName").val("Ivan");
+        $("#lastName").setValue("Ivanov");
+        $("#userEmail-wrapper #userEmail").setValue("iivanov@mail.ru");
+        $x("//label[contains(text(),'Male')]").click();
+        $x("//input[@placeholder = 'Mobile Number']").setValue("9998887766");
+        $("#dateOfBirthInput").click();
+        $x("//select[contains(@class, 'year-select')]").selectOption("1990");
+        $x("//select[contains(@class, 'month-select')]").selectOption("December");
+        $x("//div[contains(text(), '15')]").click();
+        $("#subjectsInput").setValue("English").pressEnter();
+        $x("//label[contains(text(),'Reading')]").click();
+        $("#uploadPicture").uploadFromClasspath("cat.PNG");
+        $("#currentAddress").setValue("www.LeningradSPB.ru");
+        $x("//div[@id='state']//input").setValue("Uttar Pradesh").sendKeys(Keys.ENTER);
+        $x("//div[@id='city']//input").setValue("Agra").sendKeys(Keys.ENTER);
+        $("#submit").click();
+        assertCheckResultStudentInfo("Ivan Ivanov", "Student Name");
+        assertCheckResultStudentInfo("iivanov@mail.ru", "Student Email");
+        assertCheckResultStudentInfo("Male", "Gender");
+        assertCheckResultStudentInfo("9998887766", "Mobile");
+        assertCheckResultStudentInfo("15 December,1990", "Date of Birth");
+        assertCheckResultStudentInfo("English", "Subjects");
+        assertCheckResultStudentInfo("Reading", "Hobbies");
+        assertCheckResultStudentInfo("cat.PNG", "Picture");
+        assertCheckResultStudentInfo("www.LeningradSPB.ru", "Address");
+        assertCheckResultStudentInfo("Uttar Pradesh Agra", "State and City");
+    }
 
-        registrationPage
-                .assertCheckResultStudentInfo("Ivan Ivanov", "Student Name")
-                .assertCheckResultStudentInfo("iivanov@mail.ru", "Student Email")
-                .assertCheckResultStudentInfo("Male", "Gender")
-                .assertCheckResultStudentInfo("9998887766", "Mobile")
-                .assertCheckResultStudentInfo("15 December,1990", "Date of Birth")
-                .assertCheckResultStudentInfo("English", "Subjects")
-                .assertCheckResultStudentInfo("Reading", "Hobbies")
-                .assertCheckResultStudentInfo("cat.PNG", "Picture")
-                .assertCheckResultStudentInfo("www.LeningradSPB.ru", "Address")
-                .assertCheckResultStudentInfo("Uttar Pradesh Agra", "State and City");
+    public String getValueFromResultTable(String value) {
+        return String.format("//td[text()='%s']/following-sibling::*", value);
+    }
+
+    public void assertCheckResultStudentInfo(String expected, String valueFromResultTable) {
+        assertEquals(String.format("%s", expected),
+                $x(getValueFromResultTable(String.format("%s", valueFromResultTable))).getText());
+    }
+
+    void adBlockAndFooterRemove() {
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
     }
 }
+
